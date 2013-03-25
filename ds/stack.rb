82d4f6a6cae59b14a873ba5e9b16105444a61d23
anjalicg2001 <anjalicg2001@gmail.com>
2013-03-25 08:10:42 +0000

    stack and queue

---
+++ ds/stack.rb
@@ -0,0 +1,26 @@
+#Stack implementation in Ruby
+class Sdata
+  attr_accessor :element, :ptr
+  def initialize(ele, p)
+    @element =ele #type int
+    @ptr = p #type sdata
+  end
+    
+end
+class Stack
+  attr_accessor :top
+  def initialize
+    @top=nil 
+  end
+  def push(iData) #Integer data that needs to be stored
+    
+    
+  end
+  def pop
+    unless @top==nil
+      
+    end
+  end
+  
+end
+
